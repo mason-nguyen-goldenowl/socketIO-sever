58bd195d9892365c9080b86718f325f740b6e199
MasonNguyen <mason_nguyen@mason.nguyen>
2022-02-23 16:41:17 +0700

    setup sever

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const sever = http.createServer(app);
+
+const io = new Server(sever, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Client Connected: ", socket.id);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client Disconnected", socket.id);
+  });
+});
+
+sever.listen(3838, () => {
+  console.log("SERVER RUNNING...");
+});
